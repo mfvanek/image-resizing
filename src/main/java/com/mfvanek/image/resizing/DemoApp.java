32a01b3e78fe667bae52c38a715ace648c9a32b6
Ivan Vakhrushev <ivan.vakhrushev@akvelon.com>
2018-12-11 14:10:51 +0300

    Initiated sources

---
+++ src/main/java/com/mfvanek/image/resizing/DemoApp.java
@@ -0,0 +1,8 @@
+package main.java.com.mfvanek.image.resizing;
+
+public class DemoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hi there! This is demo application for image resizing");
+    }
+}
